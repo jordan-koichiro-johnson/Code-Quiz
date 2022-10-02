27f92832cd4998c8cdd19e04465fbc433787eb96
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-10-01 17:08:48 -0700

    highscore working

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,9 @@ var incorrectPopup;
 var correctPopup;
 var correct = "0";
 var incorrect = "0";
+var initials;
+
+var highscores = [];
 // function countdown() {;
 //     var timeLeft = 30
 
@@ -21,6 +24,21 @@ var incorrect = "0";
 // }
 
 //create array of questions
+
+if (window.localStorage.getItem("score0") === null) {
+  window.localStorage.setItem("initials0", "none");
+  window.localStorage.setItem("score0", "0");
+} else {
+  for (n = 0; n < 3; n++) {
+    if (window.localStorage.getItem("initials" + n) === null) {
+    } else {
+      highscores.push({
+        initials: window.localStorage.getItem("initials" + n),
+        score: window.localStorage.getItem("score" + n),
+      });
+    }
+  }
+}
 const questionBank = [
   {
     question: "Which of these The Beatles albums released the latest",
@@ -70,11 +88,7 @@ function addSpan(x) {
           "score: " + correct + " correct " + incorrect + "incorrect";
 
         if (x === questionBank.length - 1) {
-          //end set of questions and show high score
-          //TODO: accept initials and restart
-          score.innerHTML = "";
-          quiz.innerHTML =
-            "score: " + correct + " correct " + incorrect + "incorrect";
+          questionDone();
         } else {
           //create next question on click if not last question
           addSpan(x + 1);
@@ -94,11 +108,7 @@ function addSpan(x) {
           "score: " + correct + " correct " + incorrect + "incorrect";
 
         if (x === questionBank.length - 1) {
-          //end set of questions and show high score
-          //TODO: accept initials and restart
-          score.innerHTML = "";
-          quiz.innerHTML =
-            "score: " + correct + " correct " + incorrect + "incorrect";
+          questionDone();
         } else {
           //create next question on click if not last question
           addSpan(x + 1);
@@ -106,4 +116,55 @@ function addSpan(x) {
       });
     }
   }
+}
+
+function questionDone() {
+  //end set of questions and show high score
+  //TODO: accept initials and restart
+  score.innerHTML = "";
+  quiz.innerHTML = "score: " + correct + " correct " + incorrect + "incorrect";
+  var formSubmit = document.createElement("form");
+  quiz.appendChild(formSubmit);
+  initials = document.createElement("input");
+  initials.value = "Initials";
+  initials.setAttribute("type", "input");
+  initials.onfocus = function () {
+    initials.value = "";
+  };
+
+  formSubmit.appendChild(initials);
+  var submit = document.createElement("button");
+  submit.setAttribute("type", "button");
+  formSubmit.appendChild(submit);
+  submit.textContent = "submit";
+  initials.textContent = initials;
+  var highscoreShow = document.createElement("span");
+  highscoreShow.textContent = highscores.map((x) => x.initials);
+  highscoreShow.style.background = "Blue";
+  highscoreShow.style.color = "white";
+  quiz.appendChild(highscoreShow);
+  var highscoreScore = document.createElement("span");
+  highscoreScore.textContent = highscores.map((x) => x.score);
+  highscoreScore.style.background = "red";
+  quiz.appendChild(highscoreScore);
+  submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    highscores.unshift({
+      initials: initials.value,
+      score: correct,
+    });
+    highscores.sort((x, y) => (y.score > x.score ? 1 : -1));
+    console.log(highscores);
+
+    if (highscores.length > 3) {
+      highscores.pop();
+    }
+    for (i = 0; i < highscores.length; i++) {
+      window.localStorage.setItem("initials" + i, highscores[i].initials);
+      window.localStorage.setItem("score" + i, highscores[i].score);
+    }
+    console.log(highscores);
+
+    location.reload();
+  });
 }
